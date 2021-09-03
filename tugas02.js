1dfed043faf32504251a612ca4628f620b954926
whoisraa <rairaiatlantica@gmail.com>
2021-09-03 09:34:39 +0700

    tugas 2 js

---
+++ tugas02.js
@@ -0,0 +1,11 @@
+var nomorRumah = 5;
+var alamatRumah = "Jalan Kenari";
+var rumahSaya = false;
+var masuk;
+var nomorTetangga = [3,6,9,2,8];
+
+console.log(nomorRumah);
+console.log(alamatRumah);
+console.log(rumahSaya);
+console.log(masuk);
+console.log(nomorTetangga[0]);
